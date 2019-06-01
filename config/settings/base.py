24b16eaa72a91ed402d2580fb895a120504ffffc
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2019-06-01 12:01:30 +0200

    Fix initial migration

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,7 @@ THIRD_PARTY_APPS = [
     "webpack_loader",
 ]
 LOCAL_APPS = [
+    'openfootprint.users.apps.UsersConfig',
     'openfootprint.core.apps.CoreConfig'
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -93,7 +94,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
-#AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
 #LOGIN_REDIRECT_URL = "users:redirect"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
