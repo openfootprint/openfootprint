8d8d2bcb37486dfc9f321fb36b6a97e7e7fd7161
Vincent Zimmer <vzimmer@gmail.com>
2019-06-01 12:20:34 +0200

    Clean base.html

--- frontend/scripts/app.vue
+++ frontend/scripts/app.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <div>
+      <b-nav vertical class="w-25">
+        <b-nav-item active>Active</b-nav-item>
+        <b-nav-item>Link</b-nav-item>
+        <b-nav-item>Another Link</b-nav-item>
+        <b-nav-item disabled>Disabled</b-nav-item>
+      </b-nav>
+    </div>
+    
     <router-view></router-view>
   </div>
 </template>
