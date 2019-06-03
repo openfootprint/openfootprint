d57eb62eafd12f8e081fe355ad88e7c2dfee7cf6
maelorn <ismael@pricingassistant.com>
2019-06-03 08:27:31 +0200

    Register tags in admin models

--- openfootprint/core/admin.py
+++ openfootprint/core/admin.py
@@ -5,6 +5,8 @@ admin.site.register(models.Project)
 
 admin.site.register(models.Footprint)
 
+admin.site.register(models.Tag)
+
 admin.site.register(models.Location)
 
 admin.site.register(models.Person)
